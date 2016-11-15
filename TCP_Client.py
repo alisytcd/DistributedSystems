888638a6ea6233252d553f6132c027b2b14b374e
alisytcd <alisy@tcd.ie>
2016-11-15 14:01:31 +0000

    Add files via upload
    Initial threaded server with modified client..still need to implement threadpooling

--- TCP_Client.py
+++ TCP_Client.py
@@ -12,12 +12,16 @@ serverPort = 8000
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
-sentence = 'GET /echo.php?message=hello HTTP/1.1\r\n\r\n'
-clientSocket.send(sentence)
+while True:
+#sentence = 'GET /echo.php?message=hello HTTP/1.1\r\n\r\n'
+    sentence = raw_input("Message:")
+    clientSocket.send(sentence)
+     
+
+    status= clientSocket.recv(1024)
 
 
-status= clientSocket.recv(1024)
-
+   
+    print status
 
 clientSocket.close()
-print status
